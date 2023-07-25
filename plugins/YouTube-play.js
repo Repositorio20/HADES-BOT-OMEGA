9eee726829edc4607d0f28717545336b0a1ae21a
Repositorio20 <140273977+Repositorio20@users.noreply.github.com>
2023-07-25 17:11:35 -0600

    Actualizar YouTube-play.js

--- plugins/YouTube-play.js
+++ plugins/YouTube-play.js
@@ -13,7 +13,7 @@ const yt_play = await search(args.join(" "))
 let additionalText = ''
 if (command === 'play') {
 additionalText = 'audio 🔊'
-} else if (command === 'play2') {
+} else if (command === 'playvid') {
 additionalText = 'video 🎥'}
 let texto1 = `༴⃟🌹๋ོ࣭ꦿ⁩PLAY-YouTube--⦿
 ----------------------------------------------------------------------------------------------------------
@@ -22,7 +22,7 @@ let texto1 = `༴⃟🌹๋ོ࣭ꦿ⁩PLAY-YouTube--⦿
 ➯➤📆 *Published:* ${yt_play[0].ago}
 ➯➤⌚ *Duration:* ${secondString(yt_play[0].duration.seconds)}
 ➯➤👀 *Views:* ${`${MilesNumber(yt_play[0].views)}`}
-▢⫷᭄©𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰﹏✍
+▢⫷᭄©DROID-8-MD﹏✍
 ----------------------------------------------------------------------------------------------------------`.trim()
 conn.sendMessage(m.chat, { image: { url: yt_play[0].thumbnail }, caption: texto1 }, { quoted: m })
 if (command == 'play') {
